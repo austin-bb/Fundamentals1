5f8cbb66a215da24e7f624dcdae85dead80f3737
Austinbb <austinbaumbach2@gmail.com>
2022-06-06 13:21:33 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿for (int i = 1; i <= 255; i++) {
+  Console.WriteLine(i);
+}
+
+for (int i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 != 0) {
+    Console.WriteLine("Fizz");
+  }
+  else if (i % 5 == 0 && i % 3 != 0) {
+    Console.WriteLine("Buzz");
+  }
+  else if (i % 5 == 0 && i % 3 == 0) {
+    Console.WriteLine("FizzBuzz");
+  } else {
+    Console.WriteLine(i);
+  }
+}
